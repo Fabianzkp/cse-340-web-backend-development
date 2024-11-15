8557c937679b28dd10172e5c70c2a6a0863b2a22
Fabianzkp <fabianzkp@gmail.com>
2024-11-15 17:42:28 +0100

    check2

--- server.js
+++ server.js
@@ -21,12 +21,12 @@ app.set("layout", "./layouts/layout") // not at views root
 /* ***********************
  * Routes
  *************************/
-// Add route for rendering the index.ejs file
-app.get("/", (req, res) => {
-  res.render("index", { title: "Home" })
-})
-
 app.use(static)
+
+// Index route
+app.get("/", function (req, res){
+  res.render("index", {title: "Home"})
+})
 
 /* ***********************
  * Local Server Information
