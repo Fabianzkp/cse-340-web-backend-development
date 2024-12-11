023565e4d97631830541ad46dab1aef5a8ea10f7
Fabianzkp <fabianzkp@gmail.com>
2024-12-11 19:17:54 +0100

    wk4-account

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -4,6 +4,7 @@ const baseController = {}
 baseController.buildHome = async function(req, res, next) {
   try {
     const nav = await utilities.getNav();  // This should be awaited
+    // req.flash("notice", "This is a flash message.") // This is for the flash message 
     res.render("index", { title: "Home", nav });
   } catch (err) {
     // If getNav fails, log the error and pass it to the error handler
--- server.js
+++ server.js
@@ -14,7 +14,30 @@ const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
 const utilities = require("./utilities")
 const errorRoute = require("./routes/errorRoute")
+const session = require("express-session")
+const pool = require('./database/')
+const accountRoute = require("./routes/accountRoute");
 
+/* ***********************
+ * Middleware
+ * ************************/
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
 
 /* ***********************
  * View Engine and Templates
@@ -30,6 +53,7 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
 app.use("/inv", inventoryRoute)
 app.use("/inv/error", errorRoute)
+app.use("/account", accountRoute)
 
 // Index route for homepage
 app.get("/", utilities.handleErrors(baseController.buildHome))
